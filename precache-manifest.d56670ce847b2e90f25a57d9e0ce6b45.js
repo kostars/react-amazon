e672d8497f914fa31ac6c7b9a29cb1370a72e476
Viera Kay <kostars@hotmail.co.uk>
2020-10-05 20:00:13 +0100

    your msg

---
+++ precache-manifest.d56670ce847b2e90f25a57d9e0ce6b45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593cb2fb7bbfaf4952aaaa3e526f7199",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d25910f2bd868382502c",
+    "url": "/static/css/main.d89cb6b5.chunk.css"
+  },
+  {
+    "revision": "224af955e8073204834e",
+    "url": "/static/js/2.bffadc87.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.bffadc87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d25910f2bd868382502c",
+    "url": "/static/js/main.02f2f2ec.chunk.js"
+  },
+  {
+    "revision": "db30cc81162a9a7a32b5",
+    "url": "/static/js/runtime-main.5521bc21.js"
+  }
+]);
